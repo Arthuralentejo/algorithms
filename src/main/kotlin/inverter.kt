4921085f4b640b3486db2468dbda88f5e0a3f9ea
Arthuralentejo <arthur.alentejo228@gmail.com>
2022-04-09 16:58:46 -0300

    Writing inverter function test

--- src/main/kotlin/inverter.kt
+++ src/main/kotlin/inverter.kt
@@ -1,4 +1,3 @@
 fun inverter(input : List<Any>) : List<Any> {
-    return input.indices.map { i -> input[input.size - 1 - i]
-    }
+    return input.indices.map { i -> input[input.size - 1 - i] }
 }
--- src/test/kotlin/InverterKtTest.kt
+++ src/test/kotlin/InverterKtTest.kt
@@ -1,1 +1,11 @@
-class InverterTest {}
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class InverterKtTest {
+
+    @Test
+    fun inverter(){
+        assertEquals(listOf(5,4,3,2,1), inverter(listOf(1,2,3,4,5)))
+    }
+}
